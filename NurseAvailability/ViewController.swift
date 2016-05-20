092f836635e4b555277f4965dbc5ab3064fc435a
Kiera O'Reilly <kforeilly7@hotmail.com>
2016-05-20 11:27:40 +0100

    Initial Commit

---
+++ NurseAvailability/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  NurseAvailability
+//
+//  Created by Kiera O'Reilly on 20/05/2016.
+//  Copyright © 2016 Marks and Spencer. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
